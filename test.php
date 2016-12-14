b28d2e4720b315177e496237ea7f8bc1a5d76160
BbosaH <bbosa.henry1@gmail.com>
2016-12-14 18:27:13 +0300

    Update test.php

--- test.php
+++ test.php
@@ -15,9 +15,7 @@
   `active` int(10) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;";
 
-$sql = "INSERT INTO `kabitigidi` (`id`, `first_name`, `last_name`, `gender`, `email`, `password`, `phone`, `dob`, `avatar`, `date_created`, `active`) VALUES
-(1, 'Alien', 'Ware', 'male', 'alien@gmail.com', '$2a$10$772866c65026b58bfd9b9uMo6QHTKjh.K3R3lMdfI.E', '0790164259', '1481383407', 'AlienWare1481383407.png', '1481383419', 1),
-(2, 'lujja', 'Henry', 'male', 'bbosa.henry1@gmail.com', '$2a$10$4ad18e94a231d1e09101fuWMjDroYvlOmIzYXA4nL3h', '07859695', '1456873200', 'lujjaHenry1481384319.png', '1481384319', 1);";
+$sql = "drop table kabitigidi";
   
 
 
@@ -37,21 +35,6 @@ try{
     }
     
     
-  
-    $sql = "SELECT * FROM `kabitigidi`";
-    $result = $conn->query($sql);
-    $rows = $result->fetchAll(PDO::FETCH_ASSOC);
-           
-    // Check for successful insertion
-    if ($result) {
-        echo "Wetegeleza<br/>";
-        var_dump($result);
-        echo "<br/>ngunyi chooti<br/>";
-        var_dump($rows);
-    } else {
-        echo "Enjagga ekmaulu<br/>";
-        var_dump($result);
-    }
 }catch(PDOException $ex){
     die($ex->getMessage());
 }
