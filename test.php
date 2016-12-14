ff4da50d7e0f9e3b4421d383c0d641c123c4cc52
BbosaH <bbosa.henry1@gmail.com>
2016-12-14 20:04:48 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,10 @@
 <?php
   include("include/DbConnect.php");
   
-  $sql = "ALTER TABLE `admin`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;";
+  $sql = "INSERT INTO `admin` (`id`, `first_name`, `last_name`, `gender`, `email`, `password`, `phone`, `dob`, `avatar`, `date_created`, `active`) VALUES
+(1, 'Alien', 'Ware', 'male', 'alien@gmail.com', '$2a$10$772866c65026b58bfd9b9uMo6QHTKjh.K3R3lMdfI.E', '0790164259', '1481383407', 'AlienWare1481383407.png', '1481383419', 1),
+(2, 'lujja', 'Henry', 'male', 'bbosa.henry1@gmail.com', '$2a$10$4ad18e94a231d1e09101fuWMjDroYvlOmIzYXA4nL3h', '07859695', '1456873200', 'lujjaHenry1481384319.png', '1481384319', 1);
+";
 
 //$sql = "drop table kabitigidi";
 $sql2 = "ALTER TABLE `chat`
@@ -58,44 +60,44 @@ try{
     $db = new DbConnect();
     $conn = $db->connect();
     
-    $stmt = $conn->prepare($sql);
-    $result = $stmt->execute();
+     $stmt = $conn->prepare($sql);
+     $result = $stmt->execute();
   
-    $stmt2 = $conn->prepare($sql2);
-    $result2 = $stmt2->execute();
+//     $stmt2 = $conn->prepare($sql2);
+//     $result2 = $stmt2->execute();
   
-    $stmt3 = $conn->prepare($sql3);
-    $result3 = $stmt3->execute();
+//     $stmt3 = $conn->prepare($sql3);
+//     $result3 = $stmt3->execute();
   
-    $stmt4 = $conn->prepare($sql4);
-    $result4 = $stmt4->execute();
+//     $stmt4 = $conn->prepare($sql4);
+//     $result4 = $stmt4->execute();
   
-    $stmt5 = $conn->prepare($sql5);
-    $result5 = $stmt5->execute();
+//     $stmt5 = $conn->prepare($sql5);
+//     $result5 = $stmt5->execute();
   
-    $stmt6 = $conn->prepare($sql6);
-    $result6 = $stmt6->execute();
+//     $stmt6 = $conn->prepare($sql6);
+//     $result6 = $stmt6->execute();
   
-    $stmt7 = $conn->prepare($sql7);
-    $result7 = $stmt7->execute();
+//     $stmt7 = $conn->prepare($sql7);
+//     $result7 = $stmt7->execute();
   
-    $stmt8 = $conn->prepare($sql8);
-    $result8 = $stmt8->execute();
+//     $stmt8 = $conn->prepare($sql8);
+//     $result8 = $stmt8->execute();
   
-    $stmt9 = $conn->prepare($sql9);
-    $result9 = $stmt9->execute();
+//     $stmt9 = $conn->prepare($sql9);
+//     $result9 = $stmt9->execute();
   
-    $stmt10 = $conn->prepare($sql10);
-    $result10 = $stmt10->execute();
+//     $stmt10 = $conn->prepare($sql10);
+//     $result10 = $stmt10->execute();
   
-    $stmt11 = $conn->prepare($sql11);
-    $result11 = $stmt11->execute();
+//     $stmt11 = $conn->prepare($sql11);
+//     $result11 = $stmt11->execute();
   
-    $stmt12 = $conn->prepare($sql12);
-    $result12 = $stmt12->execute();
+//     $stmt12 = $conn->prepare($sql12);
+//     $result12 = $stmt12->execute();
   
-    $stmt13 = $conn->prepare($sql13);
-    $result13 = $stmt13->execute();
+//     $stmt13 = $conn->prepare($sql13);
+//     $result13 = $stmt13->execute();
   
 //     $stmt14 = $conn->prepare($sql14);
 //     $result14 = $stmt14->execute();
