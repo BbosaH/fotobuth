03678db713087c1933dc491c849b16962fc0b5ef
BbosaH <bbosa.henry1@gmail.com>
2016-12-27 15:16:08 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,9 @@
 <?php
 include("include/DbConnect.php");
   
-$sql = "ALTER TABLE `admin`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;";
+$sql = "INSERT INTO `user` (`id`, `first_name`, `last_name`, `gender`, `email`, `password_hash`, `phone_number`, `university`, `school`, `classs`, `auth_tocken`, `avatar`, `chat_status`, `level`) VALUES
+(1, 'henry', 'Bbosa', 'female', 'bbosa.henry1@gmail.com', '$2a$10$bb7c46b506c1ccf374be4u5fNopGDYh0LzFwAzo5yBWhVsUYBgqN2', '+256 77777557', '1', '1', '1', '6325ab30f8d2b4543e637507ebc820cd', 'avatar.png', 1, 0),
+(2, 'Arnold', 'Pettersson', 'male', 'arnold.pettersson@gmail.com', '$2a$10$46af66222c8966e246c09OJyN6nl4uyIQMRtcYwc1A5A1ocwhm9Fq', '+46 790164259', '1', '1', '1', '0dcf8e049752fa9cbe04c4037c3e015b', 'avatar.png', 0, 0);";
 
 //$sql = "drop table kabitigidi";
  $sql2 = "ALTER TABLE `chat`
@@ -57,47 +58,47 @@ try{
      $stmt = $conn->prepare($sql);
      $result = $stmt->execute();
   
-    $stmt2 = $conn->prepare($sql2);
-    $result2 = $stmt2->execute();
+//     $stmt2 = $conn->prepare($sql2);
+//     $result2 = $stmt2->execute();
   
-    $stmt3 = $conn->prepare($sql3);
-    $result3 = $stmt3->execute();
+//     $stmt3 = $conn->prepare($sql3);
+//     $result3 = $stmt3->execute();
   
-    $stmt4 = $conn->prepare($sql4);
-    $result4 = $stmt4->execute();
+//     $stmt4 = $conn->prepare($sql4);
+//     $result4 = $stmt4->execute();
   
-    $stmt5 = $conn->prepare($sql5);
-    $result5 = $stmt5->execute();
+//     $stmt5 = $conn->prepare($sql5);
+//     $result5 = $stmt5->execute();
   
-    $stmt6 = $conn->prepare($sql6);
-    $result6 = $stmt6->execute();
+//     $stmt6 = $conn->prepare($sql6);
+//     $result6 = $stmt6->execute();
   
-    $stmt7 = $conn->prepare($sql7);
-    $result7 = $stmt7->execute();
+//     $stmt7 = $conn->prepare($sql7);
+//     $result7 = $stmt7->execute();
   
-    $stmt8 = $conn->prepare($sql8);
-    $result8 = $stmt8->execute();
+//     $stmt8 = $conn->prepare($sql8);
+//     $result8 = $stmt8->execute();
   
-    $stmt9 = $conn->prepare($sql9);
-    $result9 = $stmt9->execute();
+//     $stmt9 = $conn->prepare($sql9);
+//     $result9 = $stmt9->execute();
   
-    $stmt10 = $conn->prepare($sql10);
-    $result10 = $stmt10->execute();
+//     $stmt10 = $conn->prepare($sql10);
+//     $result10 = $stmt10->execute();
   
-    $stmt11 = $conn->prepare($sql11);
-    $result11 = $stmt11->execute();
+//     $stmt11 = $conn->prepare($sql11);
+//     $result11 = $stmt11->execute();
   
-    $stmt12 = $conn->prepare($sql12);
-    $result12 = $stmt12->execute();
+//     $stmt12 = $conn->prepare($sql12);
+//     $result12 = $stmt12->execute();
   
-    $stmt13 = $conn->prepare($sql13);
-    $result13 = $stmt13->execute();
+//     $stmt13 = $conn->prepare($sql13);
+//     $result13 = $stmt13->execute();
   
 //     $stmt14 = $conn->prepare($sql14);
 //     $result14 = $stmt14->execute();
   
     if ($result) {
-        echo "Kyekyo 3<br/>";
+        echo "Kyekyo 4<br/>";
         var_dump($result);
     } else {
         echo "winna<br/>";
